96fba4ab249db0a4edeeffb64d8947b7164c05e7
Mariya Titova <mariya.titova@simbirsoft.com>
2022-05-28 23:04:39 +0300

    feat: add basic config

---
+++ movies/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Main
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
